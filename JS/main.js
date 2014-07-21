5e4f2a357807f2a97f4c3c79631ea23da34781b6
bwler <Roxas_24@web.de>
2014-07-21 18:40:27 +0200

    First Attempts

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,7 @@
 //Main JS-File
+//$(document).ready(function(){$("p",index.html);});
+$(document).ready(function(){
+	$("<div><p>Durch JavaScript angehaengt.</p></div>",document).appendTo("body");
+	//d3.selectAll("p").style("color", "white");
+	//d3.select("body").style("background-color", "black");
+	});
